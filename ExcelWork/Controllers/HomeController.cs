afaeb3bb3575817d41af58afe1c61d808e979700
devensh1 <devenshpal@gmail.com>
2023-11-03 14:15:59 +0530

    first commit

---
+++ ExcelWork/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExcelWork.Models;
+using OfficeOpenXml;
+
+namespace ExcelWork.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        DataConsolidationEntities db = new DataConsolidationEntities();
+
+        public HomeController()
+        {
+            // Set the LicenseContext based on your licensing status
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial; // or LicenseContext.Commercial
+        }
+
+        public ActionResult Index()
+        {
+            List<student> data = db.students.ToList();
+
+            try
+            {
+                DataTable dt = new DataTable();
+                dt.Columns.Add("Id", typeof(int));
+                dt.Columns.Add("Name", typeof(string));
+                dt.Columns.Add("Age", typeof(string));
+                dt.Columns.Add("Department", typeof(string));
+
+                foreach (var item in data)
+                {
+                    DataRow dr = dt.NewRow();
+                    dr[0] = item.Id;
+                    dr[1] = item.Name;
+                    dr[2] = item.Age;
+                    dr[3] = item.Department;
+                    dt.Rows.Add(dr);
+                }
+                var memory = new MemoryStream();
+                using (var Excel = new ExcelPackage(memory))
+                {
+                    var Worksheet = Excel.Workbook.Worksheets.Add("Student1");
+                    Worksheet.Cells["A1"].LoadFromDataTable(dt);
+                    Worksheet.Cells["A1:AN1"].Style.Font.Bold = true;
+                    Worksheet.DefaultRowHeight = 25;
+                    Session["mywork"] = Excel.GetAsByteArray();
+                    return Json("", JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public ActionResult Download()
+        {
+            if (Session["mywork"] != null)
+            {
+                byte[] md = Session["mywork"] as byte[];
+                return File(md, "application/octet-stream", "FileManager.xlsx");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ ExcelWork/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using ExcelWork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExcelWork.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        DataConsolidationEntities db = new DataConsolidationEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(student model)
+        {
+            student obj = new student();
+            obj.Id = model.Id;
+            obj.Name = model.Name;
+            obj.Age = model.Age;
+            obj.Department = model.Department;
+            db.students.Add(obj);
+            db.SaveChanges();
+            ModelState.Clear();
+            return View("Index");
+        }
+        public ActionResult AddList()
+        {
+            var res = db.students.ToList();
+            return View(res);
+        }
+    }
+}
